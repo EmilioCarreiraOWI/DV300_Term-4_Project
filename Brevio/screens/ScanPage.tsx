cae68da7a476d339733d4990cb44558b07a4534d
EmilioCarreiraOWI <113447065+EmilioCarreiraOWI@users.noreply.github.com>
2024-10-10 16:04:35 +0200

    Integrating Google Cloud Vision API with React Native Expo | Object Detection | Tutorial and Demo
    video link: https://www.youtube.com/watch?v=iir0ezSvRLw

--- Brevio/screens/ScanPage.tsx
+++ Brevio/screens/ScanPage.tsx
@@ -1,39 +1,79 @@
-// import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-// import { launchImageLibrary } from 'react-native-image-picker';
-// import RNFS from 'react-native-fs';
-// import { callGoogleVisionApi } from '../config/googleVisionApi';
+import { useState } from 'react';
+import axios from 'axios';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
+// Or define the Label type if it's not imported
+type Label = {
+  mid: string;
+  description: string;
+};
 
 const ScanPage = () => {
-  // const [imageUri, setImageUri] = useState<string | null>(null);
-  // const [text, setText] = useState('');
-  
 
-  // const handleImagePick = () => {
-  //   launchImageLibrary({ mediaType: 'photo' }, async (response) => {
-  //     if (response.assets && response.assets.length > 0) {
-  //       const image = response.assets[0];
-  //       console.log('Image response:', image);
+  const [imageUri, setImageUri] = useState<string | null>(null);
+  const [labels, setLabels] = useState([]);
 
-  //       if (image.uri) {
-  //         console.log('Image URI:', image.uri);
-  //         setImageUri(image.uri);
-  //         const base64Image = await RNFS.readFile(image.uri, 'base64');
-  //         console.log('Base64 Image:', base64Image);
-  //         const detectedText = await callGoogleVisionApi(base64Image);
-  //         setText(detectedText);
-  //       } else {
-  //         console.error('Image URI is undefined');
-  //       }
-  //     }
-  //   });
-  // };
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error('Error picking image:', error);
+    }
+  };
+
+  const analyzeImage = async () => {
+    try {
+      if (!imageUri) {
+        alert('No image selected');
+        return;
+      }
+
+      const apiKey = '14f88c6a8428c0e2329ab4176548b3e2cd96e5a4';
+      const url = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+
+      const base64Image = await FileSystem.readAsStringAsync(imageUri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      const requestData = {
+        requests: [
+          {
+            image: {
+              content: base64Image,
+            },
+            features: [
+              {
+                type: 'LABEL_DETECTION',
+                maxResults: 5,
+              },
+            ],
+          },
+        ],
+      };
+      const apiResponse = await axios.post(url, requestData);
+      const labels = apiResponse.data.responses[0].labelAnnotations;
+      setLabels(labels);
+    } catch (error) {
+      console.error('Error analyzing image:', error);
+    }
+  };
 
   return (
     // temporary
     <View style={styles.container}>
       
-      <View style={styles.scanContainer}>
+      {/* <View style={styles.scanContainer}>
         <Text style={styles.logo}>Brevio</Text>
         <View style={styles.scanOverlay}>
           <View style={styles.cornerTopLeft} />
@@ -41,43 +81,37 @@ const ScanPage = () => {
           <View style={styles.cornerBottomLeft} />
           <View style={styles.cornerBottomRight} />
         </View>
-      </View>
-    </View>
+      </View> */}
 
-// <View style={styles.container}>
-    //   <TouchableOpacity onPress={handleImagePick}>
-    //     <Text style={styles.buttonText}>Pick an Image</Text>
-    //   </TouchableOpacity>
-    //   {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    //   {text && <Text style={styles.detectedText}>{text}</Text>}
-    // </View>
+      <Text>Google cloud vision API</Text>
+
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.scanImage} />}
+
+      <TouchableOpacity onPress={pickImage} style={styles.button}>
+        <Text>Pick an image</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={analyzeImage} style={styles.button}>
+        <Text>Analyze image</Text>
+      </TouchableOpacity>
+
+      {
+        labels.length > 0 && (
+          <View>
+            <Text>Labels:</Text>
+            {labels.map((label: Label) => (
+              <Text key={label.mid} style={styles.labelText}>{label.description}</Text>
+            ))}
+          </View>
+        )
+      }
+      
+      
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
-  // buttonText: {
-  //   color: '#F39C12',
-  //   fontSize: 20,
-  //   textAlign: 'center',
-  //   marginTop: 20,
-  // },
-  // image: {
-  //   width: 200,
-  //   height: 200,
-  //   marginTop: 20,
-  // },
-  // detectedText: {
-  //   marginTop: 20,
-  //   fontSize: 16,
-  //   color: '#000',
-  // },
-
-  // termporary
   container: {
     flex: 1,
     backgroundColor: '#2C3E50',
@@ -96,7 +130,7 @@ const styles = StyleSheet.create({
   scanImage: {
     width: '90%',
     height: '70%',
-    resizeMode: 'cover',
+    resizeMode: 'contain',
   },
   scanOverlay: {
     position: 'absolute',
@@ -145,18 +179,16 @@ const styles = StyleSheet.create({
     borderRightWidth: 5,
     borderColor: '#000',
   },
-  bottomNav: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+  button: {
     backgroundColor: '#F39C12',
-    paddingVertical: 10,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
   },
-  navButton: {
-    alignItems: 'center',
-  },
-  navIcon: {
-    width: 30,
-    height: 30,
+  labelText: {
+    color: '#fff',
+    fontSize: 16,
+    margin: 5,
   },
 });
 
